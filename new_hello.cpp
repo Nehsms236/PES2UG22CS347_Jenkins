2bfc6ae0d194edcc150930330e2c6a335963d98c
Nehsms236 <121808201+Nehsms236@users.noreply.github.com>
2025-03-13 16:40:32 +0530

    Create new_hello.cpp

---
+++ new_hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hey\n";
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
+
