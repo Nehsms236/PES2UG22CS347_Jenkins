fb099d6233a40bfdef7f4dadc7ae990af7aab16b
neha-m-s <pixelpulse236@gmail.com>
2025-03-13 16:28:33 +0530

    new

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 
 int main(void) {
+  cout << "Hey\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
+
